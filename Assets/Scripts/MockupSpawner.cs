6fb3d530b158c1e47fb7fc0819326970b45a6b76
bsolla <bsolla@ucm.es>
2020-10-19 01:18:07 +0200

    Base upload of the project

---
+++ Assets/Scripts/MockupSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MockupSpawner : MonoBehaviour
+{
+    #region ---- VARIABLES ----
+
+    #region --- PRIVATE ---
+    [SerializeField]
+    MockupButtonMovement buttonPrefab;
+
+    RhythmManager rhythmManager;
+    float bpm, crotchet, lastBeat;
+    #endregion
+    #region --- PROTECTED ---
+
+    #endregion
+    #region --- PUBLIC ---
+    #endregion
+
+    #endregion
+
+
+    #region ---- METHODS ----
+    #region --- UNITY METHODS ---
+    // Start is called before the first frame update
+    void Start()
+    {
+        rhythmManager = GameObject.FindGameObjectWithTag("RhythmManager").GetComponent<RhythmManager>();
+        bpm = rhythmManager.bpm;
+        crotchet = 60 / bpm;
+
+        //InvokeRepeating("SpawnButtons", 0.5f, 2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rhythmManager.songPosition > lastBeat + crotchet)
+        {
+            SpawnButtons();
+
+            lastBeat += crotchet;
+        }
+    }
+    #endregion
+
+    #region --- CUSTOM METHODS ---
+    void SpawnButtons()
+    {
+        Instantiate(buttonPrefab, this.transform, true);
+    }
+    #endregion
+    #endregion
+}
---
+++ Assets/Scripts/RhythmManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RhythmManager : MonoBehaviour
+{
+    #region ---- VARIABLES ----
+
+    #region --- PRIVATE ---
+
+    #endregion
+    #region --- PROTECTED ---
+
+    #endregion
+    #region --- PUBLIC ---
+    public float bpm = 60; // beats per minute, ie tempo
+    public float crotchet; // duration of a beat
+    public float songPosition; // to be established using AudioSettings.dspTime, for now its just time elapsed
+    #endregion
+
+    #endregion
+
+
+    #region ---- METHODS ----
+    #region --- UNITY METHODS ---
+    // Start is called before the first frame update
+    void Start()
+    {
+        crotchet = 60 / bpm;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        songPosition = Time.time; // not permanent!! TODO
+    }
+    #endregion
+
+    #region --- CUSTOM METHODS ---
+
+    #endregion
+    #endregion
+}
