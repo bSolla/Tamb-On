75cd3700b3dcca9cbf7dd93623ee959319eb491f
SergioJoseGC <sergiojosegc@gmail.com>
2020-11-20 19:58:15 +0100

    ButtonPrefab arreglado
    Nacho no sabe hacer Prefabs.
    Tenemos un script en una carpeta que no toca (Assets) hay que moverlo.
    Ya crea botones dinamicos en el scroll, con que mantengamos el id de la cancion podemos cargar las canciones

---
+++ Assets/MusicScroll.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MusicScroll : MonoBehaviour
+{
+    public GameObject BotonCancion;
+    public Transform Content;
+
+    private void Start()
+    {
+        //TextAsset[] songs = Resources.LoadAll<Texture>("Songs/SongName");
+        DirectoryInfo dir = new DirectoryInfo("Assets/Resources/Songs/SongName");
+        FileInfo[] songs = dir.GetFiles("*.txt");
+
+        foreach (FileInfo s in songs)
+        {
+            GameObject go = Instantiate(BotonCancion) as GameObject;
+            go.transform.SetParent(Content);
+            //go.GetComponentInChildren<Text>().Text = s.ToString();
+        }
+    }
+}
